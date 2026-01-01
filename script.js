5731eee4035dc94cb5ac16b6766d05ec80f0b19d
Dhirajkumarroshan <53140647+Dhirajkumarroshan@users.noreply.github.com>
2026-01-01 17:49:23 +0530

    Update script_image.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const products = [
-  {id:1, name:"Pure Ganga Jal", price:199, img:"https://images.unsplash.com/photo-1582719478185-2f5e4a5e1e44"},
-  {id:2, name:"Puja Thali Set", price:899, img:"https://images.unsplash.com/photo-1606293926075-69a00dbfde1c"},
-  {id:3, name:"Incense Sticks", price:149, img:"https://images.unsplash.com/photo-1609921141835-710b7fa2c8c1"},
-  {id:4, name:"Brass Diya", price:299, img:"https://images.unsplash.com/photo-1618220179428-22790b461013"},
-  {id:5, name:"Rudraksha Mala", price:499, img:"https://images.unsplash.com/photo-1612392061787-2d078b3f7c38"}
+  {id:1, name:"Pure Ganga Jal", price:199, img:"images/ganga-jal.jpg"},
+  {id:2, name:"Puja Thali Set", price:899, img:"images/diya.jpg"},
+  {id:3, name:"Incense Sticks", price:149, img:"images/incense.jpg"}
 ];
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
@@ -52,3 +50,4 @@ function renderCart(){
 }
 
 updateCart();
+
